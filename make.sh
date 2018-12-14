b2a4438f0c300f89a5b2fb5d79b0c985c013580b
Göran Krampe <goran.krampe@gmail.com>
2018-12-14 09:50:45 +0100

    Comments added

--- make.sh
+++ make.sh
@@ -1,4 +1,8 @@
-# This works on Ubuntu 18.04, 64 bit
+# This works on Ubuntu 18.04, 64 bit. Produces libodbc_ext.so
+# Install it by for example:
+# sudo cp libodbc_ext.so /usr/lib/
+# sudo ldconfig
+
 cd cpp/src
 g++ -fPIC -shared -O2 -DBUILD_REAL_64_BIT_MODE -DSIZEOF_LONG=8 -I/usr/lib/dart/include -o ../../libodbc_ext.so odbc_ext.cc odbc_ext_box.cc odbc_ext_sql.cc -lodbc
 cd ../..
